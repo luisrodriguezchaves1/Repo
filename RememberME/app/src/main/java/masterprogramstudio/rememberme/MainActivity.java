54407b07a68135dff26f100a328fcb9446a21a4c
luisrodriguezchaves1 <luigi05-3@hotmail.com>
2017-06-17 22:45:56 -0600

    al fin hay un navigation drawer

--- RememberME/app/src/main/java/masterprogramstudio/rememberme/MainActivity.java
+++ RememberME/app/src/main/java/masterprogramstudio/rememberme/MainActivity.java
@@ -1,22 +1,24 @@
 package masterprogramstudio.rememberme;
 
 import android.os.Bundle;
-import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
-    private String[] mPlanetTitles;
-    private DrawerLayout mDrawerLayout;
+
+
     private ListView mDrawerList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mPlanetTitles = new String[]{"Hoy","Perfil","Configuración","Notas","Respaldo",""};
-        mDrawerLayout=(DrawerLayout) findViewById(R.id.drawer_layout);
         mDrawerList=(ListView)findViewById(R.id.left_drawer);
-
+        final String[] main_menu_opc = getResources().getStringArray(
+                R.array.nav_options_esp);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,main_menu_opc);
+        mDrawerList.setAdapter(adapter);
     }
 }
