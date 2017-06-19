6069892754e3dd128d4e0ba0cff8cec99c054595
luisrodriguezchaves1 <luigi05-3@hotmail.com>
2017-06-18 18:53:59 -0600

    hay que arreglar esta shet de app

--- RememberME/app/src/main/java/masterprogramstudio/rememberme/MainActivity.java
+++ RememberME/app/src/main/java/masterprogramstudio/rememberme/MainActivity.java
@@ -2,20 +2,24 @@ package masterprogramstudio.rememberme;
 
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private DrawerLayout mDrawerLayout;
     private ListView mDrawerList;
-    private ActionBarDrawerToggle mDrawerToggle;
+    private Toolbar toolbar;
+    private DrawerLayout mdrawerLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mdrawerLayout=(DrawerLayout)findViewById(R.id.drawer_layout);
 
         mDrawerList=(ListView)findViewById(R.id.left_drawer);
         final String[] main_menu_opc = getResources().getStringArray(
@@ -24,5 +28,29 @@ public class MainActivity extends AppCompatActivity {
                 android.R.layout.simple_list_item_1,main_menu_opc);
         mDrawerList.setAdapter(adapter);
 
+        toolbar = (Toolbar) findViewById(R.id.tool_bar);
+        setSupportActionBar(toolbar);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            mdrawerLayout.openDrawer(mdrawerLayout);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
